40376a74ad4afa394b4005826c322511fbf7c101
vasile <elsile69@yahoo.com>
2018-05-28 20:22:01 +0300

    Nvidia configs

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,12 @@
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+  # boot.blacklistedKernelModules = [ "nouveau" ];
+  # boot.kernelParams = [ "nomodeset" "video=vesa:off" "vga=normal" ];
+  # boot.vesa = false;
+  # hardware.opengl.enable = true;
+  # hardware.opengl.driSupport32Bit = true;
+  # services.xserver.videoDrivers = [ "nvidia" "vesa" ];
   hardware.pulseaudio.enable = true;
   networking.hostName = "merlin"; # Define your hostname.
   networking.networkmanager.enable = true;
